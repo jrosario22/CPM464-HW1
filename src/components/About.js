0c7c18fb85498a23146309205dabb7868cbd3afd
Jeson Rosario <jesonrosario22@gmail.com>
2020-04-19 18:24:41 -0400

    Added small bio

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function About() {
-  return <h2>About</h2>;
+  return (
+    <div>
+      <h2>About</h2>
+      <h4>
+        I'm a graduating senior at Lehman College, double majoring in computer
+        science and mathematics
+      </h4>
+      <h4>
+        I've worked as a software engineer and teaching assistant to web
+        development bootcamps
+      </h4>
+      <h4>When I'm not problem solving, I enjoy hanging out with my dog</h4>
+    </div>
+  );
 }
