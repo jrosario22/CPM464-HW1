e32b5aad98d4a4cfa92100fe3811061751208d0b
Jeson Rosario <jesonrosario22@gmail.com>
2020-04-20 18:06:07 -0400

    Updates

---
+++ precache-manifest.9808f995ad14735fdd9c233ad09cc1bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7a41237c7ee21b885bf309e9a5049f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ba0da3a62c506076f67",
+    "url": "/static/css/main.e852f196.chunk.css"
+  },
+  {
+    "revision": "a7a577ad4ef4a240226a",
+    "url": "/static/js/2.ffd53cb5.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.ffd53cb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ba0da3a62c506076f67",
+    "url": "/static/js/main.cc57720d.chunk.js"
+  },
+  {
+    "revision": "77cfb27d24002dc68e3f",
+    "url": "/static/js/runtime-main.a835dd4b.js"
+  },
+  {
+    "revision": "c667858166eddcb810bf645a5d820bba",
+    "url": "/static/media/dog.c6678581.png"
+  },
+  {
+    "revision": "271579d591b6e3313ee9b9da5ed74999",
+    "url": "/static/media/nfl.271579d5.png"
+  }
+]);
