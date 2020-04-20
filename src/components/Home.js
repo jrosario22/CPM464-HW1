20c00c327b42efde16007e51a750b5b3357e45f6
Jeson Rosario <jesonrosario22@gmail.com>
2020-04-20 17:58:05 -0400

    Moved content down on page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "../css/Home.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className="home">
       <h2>Hello</h2>
       <h2>I am Jeson Rosario</h2>
       <h3>Software Developer</h3>
